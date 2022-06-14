6060cf460dc76f850eb09583f5dd095d02fd32a9
rehanulh <rehanulh@gmail.com>
2022-06-15 01:58:44 +0200

    Blog Project Done

---
+++ server/models/Comments.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) =>
+{
+
+    const Comments = sequelize.define("Comments", {
+    
+        commentBody : {
+            type:DataTypes.STRING,
+            allowNull: false,
+
+        },
+        username : {
+            type:DataTypes.STRING,
+            allowNull: false,
+        }
+    });
+
+
+    return Comments;
+}
